2b797f35ccb1df39aa278afddcbf0640a97b02b3
omiq <chris@chrisg.com>
2025-03-01 17:16:20 +0000

    update sprites

--- src/main.c
+++ src/main.c
@@ -1,15 +1,6 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
-
+// Raylib and SearchAndSetResourceDir
 #include "raylib.h"
-
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "resource_dir.h"	
 
 int main ()
 {
@@ -17,13 +8,14 @@ int main ()
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create the window and OpenGL context
-	InitWindow(800, 600, "Hello Raylib");
+	InitWindow(800, 600, "Raylib Demo");
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+	// Utility function from resource_dir.h to find the resources folder 
+	// and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+	// Load a sprite graphic from the resources directory
+	Texture player = LoadTexture("sprite.png");
 	
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
@@ -35,10 +27,10 @@ int main ()
 		ClearBackground(BLACK);
 
 		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+		DrawText("Raylib Demo", 200,200,20,WHITE);
 
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		DrawTexture(player, 400, 200, WHITE);
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
